32813ba01f5400a7d564c0fd71067f6d17c4c952
g35p <115426869+g35p@users.noreply.github.com>
2024-01-10 22:46:59 +0200

    Update __init__.py

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -1,12 +1,4 @@
-import json
+from .avatarcog import AvatarCog
 
-from pathlib import Path
-from redbot.core.bot import Red
-
-from .avatar import Avatar
-
-with open(Path(__file__).parent / "info.json") as fp:
-	__red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
-
-async def setup(bot: Red) -> None:
-	await bot.add_cog(Avatar())
+def setup(bot):
+    bot.add_cog(AvatarCog(bot))
