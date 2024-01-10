c83e8e99a4606f62e305087aa157510220a06691
g35p <115426869+g35p@users.noreply.github.com>
2024-01-10 22:44:04 +0200

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,34 +1,32 @@
 import discord
-from redbot.core import app_commands, commands
+from redbot.core import commands
 from redbot.core.utils.chat_formatting import bold, error
 from typing import Optional
-
 
 class Avatar(commands.Cog):
     """Get a user's avatar."""
 
-    @commands.hybrid_command(name="avatar", description="Get a user's avatar")
-    @app_commands.describe(user="The user you wish to retrieve the avatar of")
-    @app_commands.guild_only()
-    async def avatar(self, ctx: commands.Context, user: discord.Member) -> None:
-        """Returns a user's avatar as attachment.
+    @commands.command(name="avatar", description="Get a user's avatar")
+    async def avatar(self, ctx: commands.Context, user: discord.Member = None) -> None:
+        """Returns a user's avatar as an attachment.
 
-        User argument can be user mention, nickname, username, user ID.
+        The user argument can be a user mention, nickname, username, or user ID.
+        Defaults to the requester when no argument is supplied."""
+        user = user or ctx.author
 
-        Defaults to requester when no argument is supplied."""
         message = ("{author} requested the avatar of {name}.").format(author=ctx.author.mention, name=bold(user.display_name))
         if user == ctx.author:
             message = ("Here is your avatar, {author}.").format(author=ctx.author.mention)
         elif user == ctx.me:
             message = ("This is _my_ avatar, {author}!").format(author=ctx.author.mention)
         elif isinstance(ctx.channel, discord.DMChannel):
-            message = ("You requested the avatar of {name}.").format(name=bold(user.global_name))
+            message = ("You requested the avatar of {name}.").format(name=bold(user.display_name))
 
-        if isinstance(ctx.channel, discord.channel.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).attach_files:
+        if isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).attach_files:
             async with ctx.typing():
                 pfp = user.avatar if isinstance(ctx.channel, discord.channel.DMChannel) else user.display_avatar
-                fileExt = "gif" if pfp and pfp.is_animated() else "png"
-            return await ctx.send(message, file=await pfp.to_file(filename=f"pfp-{user.id}.{fileExt}"))
+                file_ext = "gif" if pfp and pfp.is_animated() else "png"
+            return await ctx.send(message, file=await pfp.to_file(filename=f"pfp-{user.id}.{file_ext}"))
         elif ctx.channel.permissions_for(ctx.guild.me).embed_links:
             return await ctx.send(message + "\n" + user.display_avatar.url)
 
