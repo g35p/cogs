ff33eb957f30207ed894d2dcbf69ca008b0aa3ea
g35p <115426869+g35p@users.noreply.github.com>
2024-01-10 22:26:08 +0200

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,40 +1,22 @@
-import discord
-from redbot.core import app_commands, commands
-from redbot.core.utils.chat_formatting import bold, error
-from typing import Optional
-
 class Avatar(commands.Cog):
-    """Get a user's avatar."""
+    """Get user's avatar."""
 
-    @commands.command(name="avatar", description="Get a user's avatar")
-    @app_commands.guild_only()
-    async def avatar(self, ctx: commands.Context, user: discord.Member = None) -> None:
-        """Returns a user's avatar as an attachment.
+    @commands.command()
+    async def avatar(self, ctx, *, user: discord.Member=None):
+        """Returns user avatar.
 
         User argument can be user mention, nickname, username, user ID.
+        Default to yourself when no argument is supplied.
+        """
+        author = ctx.author
 
-        Defaults to the requester when no argument is supplied."""
-        if user is None:
-            user = ctx.author
+        if not user:
+            user = author
 
-        message = f"{ctx.author.mention} requested the avatar of {bold(user.display_name)}."
+        avatar = user.avatar if user.avatar else user.default_avatar
 
-        if user == ctx.author:
-            message = f"Here is your avatar, {ctx.author.mention}."
-        elif user == ctx.me:
-            message = f"This is _my_ avatar, {ctx.author.mention}!"
-        elif isinstance(ctx.channel, discord.DMChannel):
-            message = f"You requested the avatar of {bold(user.name)}."
+        # Get the avatar in bytes
+        avatar_bytes = await avatar.read()
 
-        async with ctx.typing():
-            pfp = user.avatar if isinstance(ctx.channel, discord.channel.DMChannel) else user.display_avatar
-            file_ext = "gif" if pfp and pfp.is_animated() else "png"
-
-            if pfp:
-                await ctx.send(message)
-                await ctx.send(file=await pfp.to_file(filename=f"pfp-{user.id}.{file_ext}"))
-            else:
-                await ctx.send(f"{message}\n{user.display_avatar.url}")
-
-    async def red_delete_data_for_user(self, **kwargs) -> None:
-        pass
+        # Send the avatar file directly
+        await ctx.send(f"{user}'s Avatar:", file=discord.File(avatar_bytes, f"{user}_avatar{avatar.suffix}"))
