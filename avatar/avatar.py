94fa1c9abee923488e752e220d3ad1e79b99c33c
g35p <115426869+g35p@users.noreply.github.com>
2024-01-10 22:46:36 +0200

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,36 +1,19 @@
 import discord
 from redbot.core import commands
-from redbot.core.utils.chat_formatting import bold, error
-from typing import Optional
 
-class Avatar(commands.Cog):
-    """Get a user's avatar."""
+class AvatarCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    @commands.command(name="avatar", description="Get a user's avatar")
-    async def avatar(self, ctx: commands.Context, user: discord.Member = None) -> None:
-        """Returns a user's avatar as an attachment.
-
-        The user argument can be a user mention, nickname, username, or user ID.
-        Defaults to the requester when no argument is supplied."""
+    @commands.command()
+    async def avatar(self, ctx, user: discord.User = None):
+        """Retrieve user avatar as an attachment."""
         user = user or ctx.author
+        avatar_url = user.avatar_url_as(format="png")  # You can change the format if needed
+        avatar_bytes = await avatar_url.read()
 
-        message = ("{author} requested the avatar of {name}.").format(author=ctx.author.mention, name=bold(user.display_name))
-        if user == ctx.author:
-            message = ("Here is your avatar, {author}.").format(author=ctx.author.mention)
-        elif user == ctx.me:
-            message = ("This is _my_ avatar, {author}!").format(author=ctx.author.mention)
-        elif isinstance(ctx.channel, discord.DMChannel):
-            message = ("You requested the avatar of {name}.").format(name=bold(user.display_name))
+        file = discord.File(avatar_bytes, filename="avatar.png")
+        await ctx.send(file=file)
 
-        if isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).attach_files:
-            async with ctx.typing():
-                pfp = user.avatar if isinstance(ctx.channel, discord.channel.DMChannel) else user.display_avatar
-                file_ext = "gif" if pfp and pfp.is_animated() else "png"
-            return await ctx.send(message, file=await pfp.to_file(filename=f"pfp-{user.id}.{file_ext}"))
-        elif ctx.channel.permissions_for(ctx.guild.me).embed_links:
-            return await ctx.send(message + "\n" + user.display_avatar.url)
-
-        await ctx.send(error(("I do not have permission to attach files or embed links in this channel.")), ephemeral=True)
-
-    async def red_delete_data_for_user(self, **kwargs) -> None:
-        pass
+def setup(bot):
+    bot.add_cog(AvatarCog(bot))
