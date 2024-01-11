256e452fb62b095a7e6128b7f0cde5a5142aa3e2
g35p <115426869+g35p@users.noreply.github.com>
2024-01-11 09:56:21 +0200

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,15 +1,16 @@
 import discord
 from redbot.core import commands
 
-class Avatar(commands.Cog):
+class AvatarCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def avatar(self, ctx, user: discord.User = None):
+        """Get the avatar of a user."""
         user = user or ctx.author
         avatar_url = user.avatar_url_as(size=1024)
-        await ctx.send(f"{user.mention}'s avatar: {avatar_url}")
+        await ctx.send(f"Avatar of {user.display_name}: {avatar_url}")
 
 def setup(bot):
-    bot.add_cog(Avatar(bot))
+    bot.add_cog(AvatarCog(bot))
