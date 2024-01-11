9ccc4fe711848a89e8806c5b204c0a4888e56545
g35p <115426869+g35p@users.noreply.github.com>
2024-01-11 10:44:19 +0200

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,19 +1,20 @@
 # Discord
 import discord
+from discord import File
 
 # Red
 from redbot.core import commands
 
 class Avatar(commands.Cog):
-    """Get user's avatar URL."""
+    """Get user's avatar and send it as an attachment."""
 
     @commands.command()
     async def avatar(self, ctx, user: discord.Member=None):
         """
-        Returns user avatar URL.
+        Sends user avatar as an attachment.
 
         :param ctx: The command context.
-        :param user: The user whose avatar URL is to be fetched.
+        :param user: The user whose avatar is to be sent.
                      Defaults to the author if not specified.
         :type user: discord.Member
         """
@@ -23,9 +24,11 @@ class Avatar(commands.Cog):
             if not user:
                 user = author
 
-            url = user.avatar.with_static_format("png")
+            avatar_url = user.avatar.with_static_format("png")
+            avatar_data = await avatar_url.read()
 
-            await ctx.send(f"{user}'s Avatar URL: {url}")
+            avatar_file = File(avatar_data, filename="avatar.png")
+            await ctx.send(f"{user}'s Avatar:", file=avatar_file)
 
         except discord.errors.NotFound:
             await ctx.send("User not found.")
