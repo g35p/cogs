e17cd8f5ebb357474381c40f4951ddcd051e6e72
g35p <115426869+g35p@users.noreply.github.com>
2024-01-11 10:50:47 +0200

    Update avatar.py

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,22 +1,33 @@
 import discord
-from redbot.core import commands
+from redbot.core import app_commands, commands
+from redbot.core.utils.chat_formatting import bold, error
+from typing import Optional
 
 class Avatar(commands.Cog):
-    """Get user's avatar URL."""
+    """Get a user's avatar."""
 
-    @commands.command()
-    async def avatar(self, ctx, *, user: discord.Member=None):
-        """Returns user avatar URL.
+    @commands.command(name="avatar", description="Get a user's avatar")
+    @app_commands.describe(user="The user you wish to retrieve the avatar of")
+    @app_commands.guild_only()
+    async def avatar(self, ctx: commands.Context, user: discord.Member = None) -> None:
+        """Returns a user's avatar as an attachment.
 
         User argument can be user mention, nickname, username, user ID.
-        Defaults to yourself when no argument is supplied.
-        """
+
+        Defaults to the requester when no argument is supplied."""
         user = user or ctx.author
-        url = f"{user.avatar.with_static_format('png')}"
+        message = f"{ctx.author.mention} requested the avatar of {bold(user.display_name)}."
 
-        if url:
-            message = f"{user}'s Avatar URL: {url}"
-        else:
-            message = f"{user} does not have a valid avatar."
+        if user == ctx.author:
+            message = f"Here is your avatar, {ctx.author.mention}."
+        elif user == ctx.me:
+            message = f"This is _my_ avatar, {ctx.author.mention}!"
 
-        await ctx.send(message)
+        async with ctx.typing():
+            pfp = user.avatar if isinstance(ctx.channel, discord.DMChannel) else user.display_avatar
+            file_ext = "gif" if pfp and pfp.is_animated() else "png"
+
+        return await ctx.send(message, file=await pfp.to_file(filename=f"pfp-{user.id}.{file_ext}"))
+
+    async def red_delete_data_for_user(self, **kwargs) -> None:
+        pass
